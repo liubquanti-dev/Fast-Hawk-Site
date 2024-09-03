b20e23ab96e042c3161f0fb1e7fa8c33f22a9751
Oleh Liubchenko <doroleijn@gmail.com>
2024-09-03 11:55:25 +0200

    code

---
+++ docusaurus.config.js
@@ -0,0 +1,133 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+
+const config = {
+  title: 'Fast Hawk',
+  tagline: 'Безкоштовний публічний Rust сервер',
+  favicon: 'img/favicon.png',
+
+  url: 'https://fhawk.fun',
+  baseUrl: '/',
+
+  organizationName: 'White-Heart-Dev',
+  projectName: 'fast-hawk-site',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'uk',
+    locales: ['uk'],
+  },
+
+  presets: [
+    [
+      'classic',
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    ({
+      image: 'img/logo-background.png',
+      navbar: {
+        title: 'Fast Hawk',
+        logo: {
+          alt: 'Fast Hawk Logo',
+          src: 'img/logo-small.png',
+          href: '/',
+        },
+        items: [
+          {
+						href: 'https://github.com/liubquanti-dev/Fast-Hawk-Site',
+						className: 'navbar-item-github',
+						position: 'right',
+					},
+					{
+						href: 'https://discord.gg/TYs8FjvzFf',
+						className: 'navbar-item-discord',
+						position: 'right',
+					},
+					{
+						type: 'localeDropdown',
+						position: 'right',
+					}
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Інформація',
+            items: [
+
+            ],
+          },
+          {
+            title: 'Спільнота',
+            items: [
+              {
+                label: 'Discord',
+                to: 'https://discord.gg/TYs8FjvzFf',
+              },
+              {
+                label: 'Steam',
+                to: 'https://steamcommunity.com/groups/dglade',
+              },
+            ],
+          },
+          {
+            title: 'Більше',
+            items: [
+
+              {
+                label: 'GitHub',
+                to: 'https://github.com/liubquanti-dev/Fast-Hawk-Site',
+              },
+            ],
+          },
+          {
+            title: 'Інші сервери',
+            items: [
+
+              {
+                label: 'Minecraft',
+                to: 'https://mcount.fun',
+              },
+              {
+                label: 'Terraria',
+                to: 'https://jbrave.fun',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} LIUBQUANTUM GROUP.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,43 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Сервер не працює',
+    imgSrc: require('@site/static/img/grey.png').default,
+    description: (
+      <>
+        Роботу сервера було зупинено та відкладено на невідомий термін у зв'язку з відсутністю гравців. Деталі про плани на запуск сервера можна знайти в <a href="https://discord.gg/TYs8FjvzFf" target='_blank'>нашій спільноті</a>.
+      </>
+    ),
+  },
+];
+
+function Feature({imgSrc, title, description}) {
+  return (
+    <div className={clsx('col col--4')} style={{margin: 'auto'}}>
+      <div className="text--center">
+        <img src={imgSrc} className={styles.featureImg} alt={title} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
